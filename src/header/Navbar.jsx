a51d2b378a97444410c8ceb13f02367e70fe43e0
ronbonnke <ronbonnke789@gmail.com>
2023-12-11 11:12:35 +0530

    Your commit message here

--- src/header/Navbar.jsx
+++ src/header/Navbar.jsx
@@ -5,7 +5,7 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav>
-      {/* Logo */}
+     
       <a href="/">
         <img
           src="https://images.bewakoof.com/web/ic-desktop-normal-bwkf-logo.svg"
@@ -13,27 +13,28 @@ const Navbar = () => {
         />
       </a>
 
-      {/* Navigation Links */}
+   
       <a href="/">Men</a>
       <a href="/">Women</a>
       <a href="/">Mobile Covers</a>
+
+
+     
+      <div className="search-bar">
+        <input type="text" placeholder="Search..." />
+      </div>
+
+
+      <div className="cart-icon">
+        <img src="https://shop.bewakoof.com/cdn/shop/t/3/assets/icon-cart-mobile.svg?v=134184629154633135331696418236" alt="Cart" />
+        <div className="cart-count">0</div>
+      </div>
 
       <div className="login">
         <a href="/">Login</a>
       </div>
 
-      {/* Search Bar */}
-      <div className="search-bar">
-        <input type="text" placeholder="Search..." />
-      </div>
-
-      {/* Cart Icon */}
-      <div className="cart-icon">
-        <img src="path/to/cart-icon.png" alt="Cart" />
-        <div className="cart-count">0</div>
-      </div>
-
-      {/* Flag */}
+      
       <div className="flag">
         <img
           src="https://www.pngfind.com/pngs/m/12-120175_indian-flag-regional-circle-indian-flag-in-circle.png"
