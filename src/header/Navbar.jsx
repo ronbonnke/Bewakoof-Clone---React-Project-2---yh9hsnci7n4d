ca2b0c18f37cd52479c8e738238f8a5457f26377
ronbonnke <ronbonnke789@gmail.com>
2023-12-14 10:16:53 +0530

    Your commit message here

--- src/header/Navbar.jsx
+++ src/header/Navbar.jsx
@@ -1,4 +1,3 @@
-// Navbar.js
 import React from 'react';
 import './Navbar.css';
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,9 @@
-
 import React, { useState, useEffect } from 'react';
 import './Homepage.css';
 
 const Homepage = () => {
   const [clothes, setClothes] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
 
   useEffect(() => {
     const fetchBestSellers = async () => {
@@ -29,15 +29,33 @@ const Homepage = () => {
     fetchBestSellers();
   }, []);
 
+  const handleNext = () => {
+    const nextIndex = startIndex + 3;
+    if (nextIndex < clothes.data.length) {
+      setStartIndex(nextIndex);
+    }
+  };
+
+  const handlePrev = () => {
+    const prevIndex = startIndex - 3;
+    if (prevIndex >= 0) {
+      setStartIndex(prevIndex);
+    }
+  };
+
   return (
     <div className="homepage">
       <h2>Best Sellers</h2>
       <div className="slider">
-        {clothes.data && clothes.data.map((item) => (
-          <div key={item._id} className="slider-item">
-            <img src={item.displayImage} alt="abcd" />
-          </div>
-        ))}
+        <div className="slider-item" style={{ transform: `translateX(-${startIndex * (100 / 3)}%)` }}>
+          {clothes.data && clothes.data.map((item) => (
+            <div key={item._id} className="slider-img">
+              <img src={item.displayImage} alt={item.name} />
+            </div>
+          ))}
+        </div>
+        <button className="slider-arrow slider-arrow-left" onClick={handlePrev}>&lt;</button>
+        <button className="slider-arrow slider-arrow-right" onClick={handleNext}>&gt;</button>
       </div>
     </div>
   );
